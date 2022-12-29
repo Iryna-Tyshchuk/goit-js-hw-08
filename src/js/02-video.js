96cc2c383e0cd5449c8bd193364adbc97f9affd0
Iryna-Tyshchuk <ukriris93@gmail.com>
2022-12-29 16:05:43 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,10 +5,27 @@ import localStorageService from './localstorage';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(onSaveStorage, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function onSaveStorage(event) {
+  // console.log(event.seconds);
+  localStorageService.save('videoplayer-current-time', event.seconds);
+  // console.log(localStorage.getItem('videoplayer-current-time'));
+}
+
+player
+  .setCurrentTime(localStorageService.load('videoplayer-current-time'))
+  .then(function (seconds) {
+    // localStorageService.load('videoplayer-current-time');
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
